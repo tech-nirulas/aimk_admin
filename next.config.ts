1122adcc9c4137b8f79cc07346d89b943325004d
Ujjwal Sharma <ujjwalsharma11290@gmail.com>
2026-04-24 16:15:17 +0530

    Add image remote patterns to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "placehold.co",
+      port: "",
+      pathname: "/**",
+    }, {
+      protocol: "https",
+      hostname: "cdn.angelsinmykitchen.in",
+      port: "",
+      pathname: "/**",
+    }]
+  }
 };
 
 export default nextConfig;
